8c46e28080b390c4f6439af4d958bab4cd6ffb87
Haziq <haziqabiyyu@gmail.com>
2022-11-19 14:31:33 +0700

    Changed liststatik in kulkas to prioqueue

--- ADT/kulkas/driver_kulkas.c
+++ ADT/kulkas/driver_kulkas.c
@@ -1,12 +1,13 @@
 #include "kulkas.h"
 
 // cd tubes-alstrukdat-j-priv
-// gcc driver_kulkas.c kulkas.c ../matrix/matrix.c ../strlib/strlib.c ../makanan/makanan.c ../liststatik/listStatik.c ../waktu/waktu.c ../charmachine/charmachine.c ../wordmachine/wordmachine.c ../tree/tree.c -o driver_kulkas 
+// gcc driver_kulkas.c kulkas.c ../matrix/matrix.c ../strlib/strlib.c ../makanan/makanan.c ../liststatik/listStatik.c ../waktu/waktu.c ../charmachine/charmachine.c ../wordmachine/wordmachine.c ../tree/tree.c ../prioqueue/prioqueue.c -o driver_kulkas 
 // ./driver_kulkas
 
 
 int main() {
     Kulkas test;
+    infotype Ayam;
     Makanan ayam;
     String nama = createString("ayam");
     String id = createString("14");
@@ -14,10 +15,12 @@ int main() {
     ayam.id = id;
     ayam.height = 2;
     ayam.width = 3;
-    ayam.kadaluwarsa.DD =3;
-    ayam.kadaluwarsa.HH = 2;
-    ayam.kadaluwarsa.MM = 48;
+    Ayam.makanan = ayam;
+    Ayam.time.DD = 3;
+    Ayam.time.HH = 2;
+    Ayam.time.MM = 48;
 
+    infotype Bawang;
     Makanan bawang;
     String nama2 = createString("bawang");
     String id2 = createString("123");
@@ -25,10 +28,12 @@ int main() {
     bawang.id = id2;
     bawang.height = 1;
     bawang.width = 1;
-    bawang.kadaluwarsa.DD = 1;
-    bawang.kadaluwarsa.HH = 4;
-    bawang.kadaluwarsa.MM = 0;
+    Bawang.makanan = bawang;
+    Bawang.time.DD = 1;
+    Bawang.time.HH = 4;
+    Bawang.time.MM = 0;
 
+    infotype Sambal;
     Makanan sambal;
     String nama3 = createString("sambal");
     String id3 = createString("56");
@@ -36,11 +41,12 @@ int main() {
     sambal.id = id3;
     sambal.height = 4;
     sambal.width = 4;
-    sambal.kadaluwarsa.DD = 0;
-    sambal.kadaluwarsa.HH = 2;
-    sambal.kadaluwarsa.MM = 1;
+    Sambal.makanan = sambal;
+    Sambal.time.DD = 0;
+    Sambal.time.HH = 2;
+    Sambal.time.MM = 1;
 
-    printf("Penjelasan makanan (berlaku driver ini saja sebagai contoh): \n" 
+    printf("Penjelasan makanan (hanya berlaku driver ini saja sebagai contoh): \n" 
             "1. ayam,  ID = 14, height: 2, width: 3, kadaluwarsa: (3,2,48)\n"
             "2. bawang, ID = 123, height: 1, width: 1, kadaluwarsa: (1,4,0\n"
             "3. sambal, ID: 56, height: 4; width: 4, kadaluwarsa: (0,2,1)\n\n");
@@ -51,43 +57,61 @@ int main() {
     printf("\n");
     
     printf("B. INSERT TO KULKAS \n\n");
+
     printf("1. MASUKKAN AYAM KE KULKAS\n");
-    insertToKulkas(ayam, &test);
-    displayKulkas(test);
-    printf("\n");
-    printf("2. MASUKKAN BAWANG KE KULKAS\n");
-    insertToKulkas(bawang, &test);
-    displayKulkas(test);
-    printf("\n");
-    printf("3. MASUKKAN SAMBAL KE KULKAS\n");
-    insertToKulkas(sambal, &test);
-    displayKulkas(test);
-    printf("\n");
-    printf("4. MASUKKAN AYAM KE KULKAS\n");
-    insertToKulkas(ayam, &test);
-    displayKulkas(test);
-    printf("\n");
-    printf("5. MASUKKAN BAWANG KE KULKAS\n");
-    insertToKulkas(bawang, &test);
-    displayKulkas(test);
-    printf("\n");
-    printf("6. MASUKKAN SAMBAL KE KULKAS\n");
-    insertToKulkas(sambal, &test);
+    insertToKulkas(Ayam, &test);
     displayKulkas(test);
     printf("\n");
 
+    printf("2. MASUKKAN BAWANG KE KULKAS\n");
+    insertToKulkas(Bawang, &test);
+    displayKulkas(test);
+    printf("\n");
+
+    printf("3. MASUKKAN SAMBAL KE KULKAS\n");
+    insertToKulkas(Sambal, &test);
+    displayKulkas(test);
+    printf("\n");
+
+    printf("4. MASUKKAN AYAM KE KULKAS\n");
+    insertToKulkas(Ayam, &test);
+    displayKulkas(test);
+    printf("\n");
+
+    printf("5. MASUKKAN BAWANG KE KULKAS\n");
+    insertToKulkas(Bawang, &test);
+    displayKulkas(test);
+    printf("\n");
+
+    printf("6. MASUKKAN SAMBAL KE KULKAS\n");
+    insertToKulkas(Sambal, &test);
+    displayKulkas(test);
+    printf("\n\n");
+
     printf("C. REMOVE FROM KULKAS\n\n");
-    Makanan temp;
-    printf("1. REMOVE MAKANAN DENGAN ID 56 DARI KULKAS\n");
-    removeFromKulkas(&test, id3, &temp);
+    infotype temp;
+    printf("1. REMOVE MAKANAN KEDUA DI LIST DARI KULKAS\n");
+    removeFromKulkas(&test, 2-1, &temp);
     displayKulkas(test);
     printf("\n");
-    printf("2. REMOVE MAKANAN DENGAN ID 14 DARI KULKAS\n");
-    removeFromKulkas(&test, id, &temp);
+    printf("Makanan yg diremove: ");
+    printString(temp.makanan.nama);
+    printf("\nWaktu kadaluarsa: ");
+    TulisTIME(temp.time);
+    printf("\n\n");
+    
+    printf("2. REMOVE MAKANAN KELIMA DI LIST DARI KULKAS\n");
+    removeFromKulkas(&test, 5-1, &temp);
     displayKulkas(test);
     printf("\n");
-    printf("2. REMOVE MAKANAN DENGAN ID 99 DARI KULKAS\n");
-    removeFromKulkas(&test, createString("99"), &temp);
+    printf("Makanan yg diremove: ");
+    printString(temp.makanan.nama);
+    printf("\nWaktu kadaluarsa: ");
+    TulisTIME(temp.time);
+    printf("\n\n");
+
+    printf("2. REMOVE MAKANAN KE-99 DARI KULKAS\n");
+    removeFromKulkas(&test, 99-1, &temp);
     displayKulkas(test);
     printf("\n");
    
